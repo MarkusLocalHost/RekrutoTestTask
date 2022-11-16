07f12c7dad707933c1f11862cbe46db7f45d45f8
Markus <asgarot72@gmail.com>
2022-11-16 13:46:08 +0300

    initial

---
+++ main.py
@@ -0,0 +1,25 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib import parse
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        name = parse.parse_qs(parse.urlparse(self.path).query).get("name")
+        message = parse.parse_qs(parse.urlparse(self.path).query).get("message")
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b'Hello ' + name[0].encode(encoding="UTF-8") + b'! ' + message[0].encode(encoding="UTF-8") + b'!')
+
+
+def run():
+    server_address = ('', 8080)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+
+
+if __name__ == "__main__":
+    run()
